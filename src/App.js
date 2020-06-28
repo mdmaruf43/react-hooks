c3785b9a8d7ecc162febd9d1f2a4387400fca404
mdmaruf43 <marufahmedcse76@gmail.com>
2020-06-28 22:02:19 +0600

    hooks

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React from 'react';
 import './App.css'
-import HookCounter2 from './Components/HookCounter2';
-import ClassCounterTwo from './Components/ClassCounterTwo';
+import HookCounterFour from './Components/HookCounterFour';
 
 function App() {
   return (
     <div className="App">
-      {/* <ClassCounter />
-      <HookCounter /> */}
-      <HookCounter2 />
-      <ClassCounterTwo />
+      <HookCounterFour />
     </div>
   );
 }
